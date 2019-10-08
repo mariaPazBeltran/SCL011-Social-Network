428585462653cc82c1978e7c919ade9c2ba2f65b
María Beltrán <mariabeltra.98@gmail.com>
2019-10-08 15:36:16 -0300

    boilerplate

---
+++ src/main.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCNhZ597mAfgrM58ySiwn8soFLITj1qC6E",
+    authDomain: "scl011-social-network-go.firebaseapp.com",
+    databaseURL: "https://scl011-social-network-go.firebaseio.com",
+    projectId: "scl011-social-network-go",
+    storageBucket: "",
+    messagingSenderId: "880878261795",
+    appId: "1:880878261795:web:7845d6bf7ff12455ed0396",
+    measurementId: "G-NXB0FE95WC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
