043dba6d6f90d2e5145331fcba195cfe46d8577b
María Beltrán <mariabeltra.98@gmail.com>
2019-10-08 17:06:35 -0300

    btn-submit

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@
  `<label id="labelEmail">ingresa tu email</label>`+
  `<input type="email" id="emailUser" placeholder="xxx@example.com">` +
  `<label id="labelPassword">contraseña</label>`+
- `<input type="password" id="passwordUser"  placeholder="ingresa tu contraseña">`
+ `<input type="password" id="passwordUser"  placeholder="ingresa tu contraseña">`+
+ `<button type="submit" id="btn-submit">Ingresar</button>`
  
 
 
