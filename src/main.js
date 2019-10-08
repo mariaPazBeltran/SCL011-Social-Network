d03dd458e67ed4cccd3a80dbd4904506d44b2594
María Beltrán <mariabeltra.98@gmail.com>
2019-10-08 18:11:02 -0300

    imgreso de usuario

--- src/main.js
+++ src/main.js
@@ -9,7 +9,51 @@
  `<label id="labelPassword">contraseña</label>`+
  `<input type="password" id="passwordUser"  placeholder="ingresa tu contraseña">`+
  `<button type="submit" id="btn-submit">Ingresar</button>`
- 
-
-
   container.appendChild(loginContainer).innerHTML
+
+  ///
+  const btnSubmit = document.getElementById("btn-submit")
+  console.log(btnSubmit)
+  btnSubmit.addEventListener('click', ()=>{
+   let userEmail = document.getElementById("emailUser").value
+  let userPassword = document.getElementById("passwordUser").value
+  console.log(userEmail)
+  console.log(userPassword)
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    console.log(errorCode)
+    var errorMessage = error.message;
+    console.log(errorMessage)
+    // ...
+    alert("tu usuario o contraseña son incorrectos")
+  });
+  
+  })
+  
+
+
+
+  /*//// registro
+  let newUsers = document.createElement("div")
+  newUsers.className="new-Users"
+  newUsers.innerHTML= 
+  `<label id="labelEmailRegister">ingresa tu email</label>`+
+ `<input type="email" id="emailRegister" placeholder="xxx@example.com">` +
+ `<label id="labelPasswordRegister">contraseña</label>`+
+ `<input type="password" id="passwordRegister"  placeholder="ingresa tu contraseña">`+
+ `<button type="submit" id="btn-submitRegister">Ingresar</button>`
+container.appendChild(newUsers).innerHTML
+ let btnrRegister= document.getElementById("btn-submitRegister");
+ btnrRegister.addEventListener('click', ()=>{
+  let userEmail = document.getElementById("emailRegister").value
+  let userPassword = document.getElementById("passwordRegister").value
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+ })*/
