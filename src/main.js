a83ff8bad02f99ab7f3e28d51021df7ec56372f3
María Beltrán <mariabeltra.98@gmail.com>
2019-10-11 18:19:09 -0300

    subiendo informacion a la base de datos

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
  // se traen las cosas que se necesitan desde el inicio
  import {viewInit} from './views/screenInit.js';
+ import {timelineView} from './views/screenTimeline.js';
  const init = () => {
      //la primera vista que se abre
   viewInit()
+  timelineView()
     
   }
   // aquí le decimos que cada vez que la pagina cargue de realizar la funcion init
